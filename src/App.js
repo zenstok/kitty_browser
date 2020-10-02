40e162aa2d02cce265ee19b21e1e1cb338fbdf99
Rares Tarabega <rares.tarabega@digitalmob.ro>
2020-10-02 12:41:08 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React, { Component } from 'react';
-import { DrizzleProvider } from 'drizzle-react'
+import React, {Component} from 'react';
+import {DrizzleProvider} from 'drizzle-react'
 import Loading from './containers/Loading';
 import Browser from './components/Browser';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 
 class App extends Component {
-  render() {
-    const drizzleOptions = {
-      contracts: []
-    };
+    render() {
+        const drizzleOptions = {
+            contracts: []
+        };
 
-    return (
-      <DrizzleProvider options={drizzleOptions}>
-        <Loading>
-          <Browser />
-        </Loading>
-      </DrizzleProvider>
-    );
-  }
+        function Home() {
+            return <h2>This is the homepage</h2>
+        }
+
+        function Kitty() {
+            return <DrizzleProvider options={drizzleOptions}>
+                <Loading>
+                    <Browser/>
+                </Loading>
+            </DrizzleProvider>
+        }
+
+        return (
+            <Router>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/kitty">Kitty</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                    <Route path="/kitty">
+                        <Kitty/>
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
