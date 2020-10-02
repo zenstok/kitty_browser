ff2b7da4b507d3143bde6d5fe5d1240898d4c3a8
Rares Tarabega <rares.tarabega@digitalmob.ro>
2020-10-02 10:40:10 +0300

    added fetch random kitty btn

--- src/components/KittyForm/KittyForm.js
+++ src/components/KittyForm/KittyForm.js
@@ -1,40 +1,46 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import {Formik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 import './KittyForm.css';
 
 const ValidationSchema = Yup.object().shape({
-  kittyId: Yup.number()
-    .integer()
-    .min(1, 'Id must be at least 1!')
-    .required('Kitty ID is required!'),
+    kittyId: Yup.number()
+        .integer()
+        .min(1, 'Id must be at least 1!')
+        .required('Kitty ID is required!'),
 });
 
-export const KittyForm = ({onSubmit}) => (
-  <div className='kitty-form'>
-    <h3>Kitty ID:</h3>
-    <Formik
-      initialValues={{
-        kittyId: '',
-      }}
-      validationSchema={ValidationSchema}
-      onSubmit={values => {
-        onSubmit(values.kittyId);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <div className="form-group">
-            <Field name="kittyId" />
-            {errors.kittyId && touched.kittyId ? (
-              <div className="form-errors">{errors.kittyId}</div>
-            ) : null}
-          </div>
-          <button type="submit">FIND KITTY</button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+export const KittyForm = ({onSubmit}) => {
+    const insertRandomKittyId = (setFieldValue, onSubmit) => {
+        let randomKittyId = Math.floor(Math.random() * 999999) + 1;
+        setFieldValue('kittyId', randomKittyId);
+        onSubmit(randomKittyId)
+    }
+    return <div className='kitty-form'>
+        <h3>Kitty ID:</h3>
+        <Formik
+            initialValues={{
+                kittyId: '',
+            }}
+            validationSchema={ValidationSchema}
+            onSubmit={values => {
+                onSubmit(values.kittyId);
+            }}
+        >
+            {({setFieldValue, errors, touched}) => (
+                <Form>
+                    <div className="form-group">
+                        <Field name="kittyId"/>
+                        {errors.kittyId && touched.kittyId ? (
+                            <div className="form-errors">{errors.kittyId}</div>
+                        ) : null}
+                    </div>
+                    <button type="submit">FIND KITTY</button>
+                    <button type="button" onClick={insertRandomKittyId.bind(null, setFieldValue, onSubmit)}>FETCH RANDOM KITTY</button>
+                </Form>
+            )}
+        </Formik>
+    </div>
+};
 
 export default KittyForm;
